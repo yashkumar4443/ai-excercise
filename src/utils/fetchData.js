3a63b108b7bc3413f1d796a90760bc74b437b043
yashkumar44431 <yashkumar44431@gmail.com>
2025-05-01 16:50:25 +0530

    Changes

---
+++ src/utils/fetchData.js
@@ -0,0 +1,25 @@
+export const exerciseOptions = {
+	method: 'GET',
+	hostname: 'exercisedb.p.rapidapi.com',
+	port: null,
+	path: '/exercises/bodyPartList',
+	headers: {
+		'x-rapidapi-key': '3e8c41a270msh1fae59ef7270f14p1cbd79jsn0a7aaf5063b7',
+		'x-rapidapi-host': 'exercisedb.p.rapidapi.com'
+	}
+};
+
+export const youtubeOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+    'X-RapidAPI-Key': 'f0021db587msh781fb1cbef39856p11c183jsn45521d5d1c85',
+  },
+};
+
+export const fetchData = async (url, options) => {
+  const res = await fetch(url, options);
+  const data = await res.json();
+
+  return data;
+};
